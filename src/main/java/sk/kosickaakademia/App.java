51d023fcd4454659facdf5fe78d8f53d0b305c50
JakubDeco <jakub.deco@kosickaakademia.sk>
2021-04-19 11:14:26 +0200

    adding MongoDBImpl.insertTask

--- src/main/java/sk/kosickaakademia/App.java
+++ src/main/java/sk/kosickaakademia/App.java
@@ -1,10 +1,21 @@
 package sk.kosickaakademia;
 
 
-public class App 
+import com.google.gson.JsonObject;
+import sk.kosickaakademia.database.MongoDBImpl;
+
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        MongoDBImpl database = new MongoDBImpl();
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("name","povysavat");
+        jsonObject.addProperty("priority",1);
+        jsonObject.addProperty("done",false);
+        jsonObject.addProperty("timeEstimate",1.5);
+
+        database.insertTask(jsonObject);
     }
 }
--- src/main/java/sk/kosickaakademia/database/MongoDBImpl.java
+++ src/main/java/sk/kosickaakademia/database/MongoDBImpl.java
@@ -14,15 +14,24 @@ public class MongoDBImpl implements MongoDB {
     private MongoDatabase mongoDbs;
     private MongoCollection<Document> mongoColl;
 
+    @Override
+    public void insertTask(JsonObject jsonObject) {
+        if (jsonObject == null)
+            return;
+
+        try {
+            getConnection();
+            Document document = Document.parse(jsonObject.toString());
+            mongoColl.insertOne(document);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private void getConnection() throws Exception{
         mongoClient = new MongoClient();
         mongoDbs = mongoClient.getDatabase("taskOrganiser");
         mongoColl = mongoDbs.getCollection("task");
-    }
-
-    @Override
-    public void insertTask(JsonObject jsonObject) {
-
     }
 
     @Override
